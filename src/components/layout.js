739ce4abb7e4f912fadbe4df05dba9ea8c5854a4
Filippo Conforti <filippo@commercelayer.io>
2019-09-27 10:23:05 +0200

    Upgraded gatsby-plugin-commercelayer to v1.2.7

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,32 @@
-import React from "react"
-import * as CLayer from 'commercelayer-react'
-import { Link } from "gatsby"
-import "./layout.css"
+import React from "react";
+import * as CLayer from "commercelayer-react";
+import { Link } from "gatsby";
+import "./layout.css";
 
-export default ({ children }) => <div>
-  <ul>
-    <li><Link to="/">Category page</Link></li>
-    <li><Link to="/product-page-radio">Product page (radio)</Link></li>
-    <li><Link to="/product-page-select">Product page (select)</Link></li>
-  </ul>
-  {children}
-  <CLayer.Config
-    baseUrl="https://static-commerce.commercelayer.io"
-    clientId="351020e9c84f2076755083f08bfe8e47365a76395db1059c3219c37abff86534"
-    marketId="185"
-    countryCode="US"
-    languageCode="en"
-    cartUrl="https://example.com/cart"
-    returnUrl="https://example.com/return"
-    privacyUrl="https://example.com/privacy"
-    termsUrl="https://example.com/terms" />
-
-</div>
+export default ({ children }) => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/">Category page</Link>
+      </li>
+      <li>
+        <Link to="/product-page-radio">Product page (radio)</Link>
+      </li>
+      <li>
+        <Link to="/product-page-select">Product page (select)</Link>
+      </li>
+    </ul>
+    {children}
+    <CLayer.Config
+      baseUrl="https://static-commerce.commercelayer.io"
+      clientId="351020e9c84f2076755083f08bfe8e47365a76395db1059c3219c37abff86534"
+      marketId="185"
+      countryCode="US"
+      languageCode="en"
+      cartUrl="https://example.com/cart"
+      returnUrl="https://example.com/return"
+      privacyUrl="https://example.com/privacy"
+      termsUrl="https://example.com/terms"
+    />
+  </div>
+);
